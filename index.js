cab74a31142662b0955a47234878da8e6f6ad635
David <Muchahi 853@gmail.com>
2024-06-17 12:59:04 +0300

    fInished

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser="Grace Hopper";
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage);
+const excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+console.log(excitedWelcomeMessage);
+const shortGreeting=`Welcome, ${currentUser.charAt(0)}!`;
+console.log(shortGreeting)
